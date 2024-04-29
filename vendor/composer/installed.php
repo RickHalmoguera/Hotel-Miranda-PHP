bc43d4499904176b15e54666c10bdd29cbc43426
Ricardo Halmoguera <halmoguerar@gmail.com>
2024-04-29 19:49:00 +0200

    ADD: composer installed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ba34afd9b7305efc37b14d85f3e18b35d82b53c3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ba34afd9b7305efc37b14d85f3e18b35d82b53c3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'eftec/bladeone' => array(
+            'pretty_version' => '4.13',
+            'version' => '4.13.0.0',
+            'reference' => 'a24ce564a681fa3e37b2490952915c403ee9e56a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../eftec/bladeone',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
